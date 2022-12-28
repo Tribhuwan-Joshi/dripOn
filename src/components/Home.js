33aa48ae7551bc491e12d3460f77b5221810d7fc
Tribhuwan <mathincosmos@gmail.com>
2022-12-28 18:27:53 +0530

    minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,23 @@ import drip from "../assets/dripGoku.jpg";
 
 const Home = () => (
   <>
-    <div className="flex font-['Cinzel']  font-semibold">
+    <div className="flex bg-red-200 flex-1 font-['Cinzel'] tracking-wider  font-semibold">
       <div className="flex flex-col justify-center items-center">
-        <h1 className="text-[50px] bold w-[70%] text-center">
+        <h1 className="text-4xl min-w-[300px] md:text-[45px] md:leading-normal bold w-[70%] font-semibold text-center">
           GET THE REAL DRIP THAT DEFINES YOU !{" "}
         </h1>
         <br />
-        <button className="border border-black text-2xl p-4 hover:text-white hover:bg-gray-500 w-[20%] min-w-[100px] mx-auto ">
+        <button className="border transition hover:scale-125 duration-300 ease-in-out border-black text-2xl p-4 hover:text-white hover:bg-[#321F28] w-[20%] min-w-[100px] mx-auto ">
           {" "}
           Buy Now
         </button>
       </div>
 
-      <img src={drip} alt="drip" className="h-full ml-auto" />
+      <img
+        src={drip}
+        alt="drip"
+        className="h-[60%] md:h-[90%] self-center  ml-auto "
+      />
     </div>
   </>
 );
