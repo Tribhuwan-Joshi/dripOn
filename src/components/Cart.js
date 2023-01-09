35cba97d89046e6aa079e6695d14d47ac1a1beda
Tribhuwan <mathincosmos@gmail.com>
2023-01-09 20:12:02 +0530

    design cart items

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -20,7 +20,7 @@ function Cart() {
         <h1 className="text-4xl font-mono font-semibold text-center ">
           Your Cart Items
         </h1>
-        <div className="bg-gray-400   p-2  grid grid-rows-[auto_repeat(2,1fr)] grid-cols-[repeat(auto-fit,1fr)] md:grid-cols-3 gap-6">
+        <div className="bg-gray-400   p-2  grid grid-rows-[repeat(2,1fr)] grid-cols-[repeat(auto-fit,1fr)] md:grid-cols-3 gap-6">
           {hoodies.map((h) => {
             return (
               <div
@@ -59,9 +59,46 @@ function Cart() {
               </div>
             );
           })}
-          
+          {hoodies.map((h) => {
+            return (
+              <div
+                key={h.name}
+                className=" flex flex-col  border border-black flex-1 h-[400px]  "
+              >
+                <h2 className="bg-[#704F4F] text-white  tracking-wider text-center p-1 text-lg font-bold h-auto">
+                  {h.name}
+                </h2>
+                <img
+                  src={h.src}
+                  alt={h.name}
+                  className=" flex-1  h-[80%] object-cover"
+                />
+                <div className="flex text-white bg-[#704F4F] border-t-2 border-black items-center flex-grow-0 flex-wrap justify-between flex-1">
+                  <h2 className="tracking-wider text-xl text-black bg-yellow-200">
+                    $9.99
+                  </h2>
+                  <div className="flex-1 flex justify-center items-center gap-2  ">
+                    {" "}
+                    <button className="bg-gray-200  font-serif w-[30px] h-[80%] text-black text-lg">
+                      -
+                    </button>{" "}
+                    <input
+                      type="number"
+                      className="h-[60%] text-center text-black w-[40%] font-mono"
+                    />
+                    <button className="bg-gray-200 font-serif w-[30px] h-[80%] text-black text-lg">
+                      +
+                    </button>
+                  </div>
+                  <button className="bg-red-600 px-1 text-sm font-sans">
+                    Remove from Cart
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
-      </div> 
+      </div>
       <div className="w-[25vw] checkout bottom-0   fixed right-0 h-[90vh]  bg-gray-200  flex flex-col pt-4 gap-12 shadow-lg  shadow-gray-700">
         <h1 className="text-3xl lg:text-4xl text-center underline underline-offset-4 font-semibold mt-4">
           {" "}
