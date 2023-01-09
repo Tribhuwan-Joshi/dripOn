aaa8b22fd5e14b017ffe025008991a4e9f330364
Tribhuwan <mathincosmos@gmail.com>
2023-01-09 12:06:13 +0530

    add checkout card

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,3 +1,5 @@
+import github from "../assets/github.png"
+
 function Cart() {
   return (
     <div className="bg-[#ee9e9e] flex-1 flex">
@@ -6,7 +8,7 @@ function Cart() {
           Your Cart Items
         </h1>
       </div>
-      <div className="w-[25vw] bg-gray-200 flex flex-col p-4  gap-12 shadow-md h-full shadow-amber-700">
+      <div className="w-[25vw] bg-gray-200  flex flex-col pt-4 gap-12 shadow-lg h-full shadow-gray-700">
         <h1 className="text-4xl text-center underline underline-offset-4 font-semibold mt-4">
           {" "}
           Net Amount
@@ -14,13 +16,14 @@ function Cart() {
         <h1 className="text-2xl font-semibold w-max mx-auto break-words">
           $ 20000
         </h1>
-        <button className=" bg-green-500  transition duration-300  hover: hover:scale-110 ease-in-out w-[80%] py-3 text-white tracking-wider font-bold text-2xl border-black border mx-auto">
+        <button className=" bg-green-600 active:bg-green-700  transition duration-300   hover:scale-110 ease-in-out w-[70%] py-3 text-white tracking-wider font-bold text-2xl border-black border mx-auto">
           {" "}
           <span className="pay"> Pay Now</span>
         </button>
-        <button className=" bg-red-500 transition duration-300 hover:scale-110 w-[80%]  ease-in-out py-3  text-white font-bold tracking-wider text-2xl border-black border mx-auto">
+        <button className=" bg-red-600 active:bg-red-700 transition duration-300 hover:scale-110 w-[70%]  ease-in-out py-3  text-white font-bold tracking-wider text-2xl border-black border mx-auto">
           <span className="empty"> Empty Cart</span>
         </button>
+        <div className="bg-black mt-auto flex items-center justify-center  h-[15vh] text-white">Lorem Ispeum</div>
       </div>
     </div>
   );
